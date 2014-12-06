90e8dfb41be32036c8f1b3710930244e1c4f7ed6
Terune <sbj009@naver.com>
2014-12-06 18:15:23 +0900

    basic Parser is working Ver.1.0(2014.12.6) should add json transform and save data.

--- xmlparser/src/cooktalk/Config.java
+++ xmlparser/src/cooktalk/Config.java
@@ -2,6 +2,7 @@ package cooktalk;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.List;
 
 import org.jdom2.Element;
 
@@ -17,14 +18,26 @@ public class Config {
 	public static void setupConfig() {
 		
 		XmlParser config = new XmlParser("source/20141015_093128_김슬기_F29_log.xml");
-		String[] s = config.getText("turn");
-		Element turnin= config.getXmlElement("turn");
+		String[] s = config.getText("turn","act_tag");
+		Element root = config.getXmlRoot();
+		Element turnin= config.getXmlElement(root,"talksori_log");
+		
+		
 		//turnlist = config.getElement("turn");
 		if (s != null) {
 			serverIp = s[0];
 		}
 	//	Element result= turnin("act_tag");
-		System.out.println(s[1]);
-		System.out.println(turnin);
+		//System.out.println(s[1]);
+		List<Element> itemList = turnin.getChildren("turn");
+		
+		for(Element turns : itemList){
+			Element turnin2= turns.getChild("system");
+		//	Element turnin3= turnin2.getChild("system");
+			//Element turnin4= turnin3.getChild("act_tag");
+			System.out.println("결과:"+turnin2.getChildText("act_tag"));
+		}
+			
+		//System.out.println(turnin);
 	}
 }
